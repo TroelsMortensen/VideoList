607e4f239eea62b909ec9ffef8772e9ea9379fa1
Troels Mortensen <tromortensen@hotmail.com>
2019-12-19 21:07:47 +0100

    Adding singleton

--- src/pagegenerator/Main.java
+++ src/pagegenerator/Main.java
@@ -1,8 +1,12 @@
 package pagegenerator;
 
+import java.sql.Timestamp;
+
 public class Main {
 
     public static void main(String[] args) {
         new Generator().run(new TreeStructureBuilder());
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println("Finished: " + timestamp);
     }
 }
